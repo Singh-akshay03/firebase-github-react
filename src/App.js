46605232fcde9c1da021fcbb8b78f30660cc89d8
akshay singh <akshay03.singh03@gmail.com>
2023-02-12 17:46:58 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserContext } from "./context/UserContext";
 import { initializeApp } from 'firebase/app';
@@ -11,16 +11,16 @@ import LogIn from './Components/LogIn'
 import Signup from './Components/Signup'
 import PageNotFound from './Components/Home'
 import Header from "./layout/Header";
-import Footer from "./layout/Footer";
+// import Footer from "./layout/Footer";
 
 import firebaseConfig from './config/firebaseConfig';
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 
 //init firebase
 
-const firebaseApp=initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-const auth = getAuth(firebaseApp);
+// const auth = getAuth(firebaseApp);
 
 function App() {
   const [user, setUser] = useState(null);
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,6 @@
 
 
-export default {
+const   firebaseConfig= {
     apiKey: "AIzaSyDmfITFDaTmVw-mZF5M58MFmIrOHe2TVR4",
     authDomain: "fir-githubapp.firebaseapp.com",
     projectId: "fir-githubapp",
@@ -9,3 +9,5 @@ export default {
     appId: "1:369924415333:web:86bab74d9cffd8f2ad0b8c",
     measurementId: "G-F0QJ75B8YH"
   };
+
+  export default firebaseConfig;
